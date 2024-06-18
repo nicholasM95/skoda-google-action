a3b03b8707617c5d7a294b997c3919989662d07d
Nicholas Meyers <nicholas.meyers@hotmail.com>
2024-06-18 17:23:52 +0200

    chore(deps): update terraform modules

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -5,7 +5,7 @@ resource "random_uuid" "vault_secret_id" {
 }
 
 module "vault" {
-  source     = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault?ref=v1.1.6"
+  source     = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault?ref=v1.1.7"
   role_id    = random_uuid.vault_role_id.result
   secret_id  = random_uuid.vault_secret_id.result
   vault_path = "skoda-google-actions"
@@ -21,14 +21,14 @@ module "vault" {
 }
 
 module "dns" {
-  source    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/dns-cloudflare?ref=v1.1.6"
+  source    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/dns-cloudflare?ref=v1.1.7"
   ip        = var.server_ip
   name      = "skoda-google-actions"
   host_name = var.host_name
 }
 
 module "application" {
-  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.1.6"
+  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.1.7"
   image_tag        = var.image_tag
   application_name = var.name
   namespace_name   = var.namespace
