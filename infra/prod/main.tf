2a5defed20619d46ca2b5dac08f167b6381260e6
Nicholas Meyers <nicholas.meyers@hotmail.com>
2025-06-19 18:58:37 +0200

    chore: inject secrets from vault

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,13 +1,21 @@
 module "vault" {
-  source     = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault?ref=v1.8.19"
+  source     = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault?ref=v1.10.4"
   vault_path = var.name
 }
 
 module "application" {
-  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.8.19"
+  source           = "git::https://github.com/nicholasM95/terraform-modules.git//modules/k8s-helm-release?ref=v1.10.4"
   image_tag        = var.image_tag
   application_name = var.name
   namespace_name   = var.namespace
   helm_path        = "../../application"
   docker_config    = var.docker_config
+}
+
+module "vault_connection" {
+  depends_on                = [module.vault]
+  source                    = "git::https://github.com/nicholasM95/terraform-modules.git//modules/vault-k8s?ref=v1.10.4"
+  vault_path                = var.name
+  service_account_name      = var.name
+  service_account_namespace = var.namespace
 }
